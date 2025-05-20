3b0f1965196a45e894e8b6934414a69f16be3985
AGATA CECILIA <ceciliacesquim@gmail.com>
2025-05-20 18:23:30 -0300

    primeiro_commit

---
+++ src/app/Pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './style_Portfolio.css';
+
+import logoBia from '../../logoBia.png';
+  
+export default function Portfolio (){
+    <div>
+        <h1>Seja bem-vindo ao Flightly!</h1>
+    </div>
+}
